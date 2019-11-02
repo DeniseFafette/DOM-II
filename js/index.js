1dcec4d0bb05e743c9b01b0da30bed9e6d6442d4
DeniseFafette <denisefafette@gmail.com>
2019-11-02 12:22:26 -0700

    added event listener to scale up cta image

--- js/index.js
+++ js/index.js
@@ -11,8 +11,15 @@
 // * [ ] `select`
 // * [ ] `dblclick`
 
-let h1 = document.querySelector('h1');
+let h1 = document.querySelector('.logo-heading');
 
 h1.addEventListener('click', event => {
     event.target.style.color = 'blue'
+    event.target.style.backgroundColor = 'yellow'
+});
+
+let imageScale = document.querySelector('img');
+
+imageScale.addEventListener('dblclick', event => {
+    event.target.style.transform = 'scale(1.8)'
 });

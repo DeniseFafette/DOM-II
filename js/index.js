b91cfc512bbb08d83f4adbdba4ea0d7c639a53f2
DeniseFafette <denisefafette@gmail.com>
2019-11-02 09:08:01 -0700

    added click event to h1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+
+// * [ ] `mouseover`
+// * [ ] `keydown`
+// * [ ] `wheel`
+// * [ ] `drag / drop`
+// * [ ] `load`
+// * [ ] `focus`
+// * [ ] `resize`
+// * [ ] `scroll`
+// * [ ] `select`
+// * [ ] `dblclick`
+
+let h1 = document.querySelector('h1');
+
+h1.addEventListener('click', event => {
+    event.target.style.color = 'blue'
+});
